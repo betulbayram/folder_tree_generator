1736bc919ae087cbcfa186405789cbd6d679a3a4
betulbayram <betulforpc@gmail.com>
2023-02-18 01:21:41 +0300

    fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 class Generator:
     def __init__(self):
-        self.tree_path = "path_to_your_folder" #TODO
+        self.tree_path = "C:/Users//betul//Documents//CPP" #TODO
         self.dirs = os.listdir(self.tree_path)
         self.file = open("tree.txt", "w")
         self.ignore = [".exe", ".git", ".gitignore", ".vscode"]
@@ -10,7 +10,7 @@ class Generator:
         self.flag = 0
         self.flag2 = 0
     def write_to_txt(self, base, files):
-
+        base = base.split("\\")[-1]
         self.file.write("\n" + (self.counter) * "\t" + "|*** " +  base)
         for f in files:
             self.flag2 = 0
@@ -23,6 +23,7 @@ class Generator:
 
 
     def generate(self):
+        roots = []
         for root, dirs, files in os.walk(self.tree_path):
             self.flag = 0
             for sub in self.ignore:
@@ -31,16 +32,17 @@ class Generator:
                     self.flag = 1
                     continue
             if(self.flag == 0):
-                print(self.counter)
+
+                
                 if self.counter == 1:
                     self.write_to_txt(root.split("//")[-1], files)
                 else:
+                    self.counter = len(root.split("\\"))
                     self.write_to_txt(root.split("\\")[-1], files)
 
-                self.counter = self.counter + 1
+            self.counter = self.counter + 1
 
 if __name__ == "__main__":
 
     g = Generator()
     g.generate()
-
